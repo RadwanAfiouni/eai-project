2340ca68835e5b4818b72be86b2b5f0a4f85474d
Radwan Afiouni <49821109+RadwanAfiouni@users.noreply.github.com>
2022-10-28 21:17:43 +0300

    Initial commit

---
+++ src/main/java/com/eai/projet/ProjetApplication.java
@@ -0,0 +1,14 @@
+package com.eai.projet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//todo: rename accordingly
+@SpringBootApplication
+public class ProjetApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjetApplication.class, args);
+    }
+
+}
